eaf2ed5955708be0d5d171be4e9cdb09c7eb9854
Rizwan Ashraf <rizwanashraf@rizwans-mini.lan>
2024-03-04 18:34:18 +0000

    title and link update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -112,7 +112,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0A66C2]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://www.linkedin.com/company/create-web-recruitment/'
+              href='https://www.linkedin.com/company/create-jobs-recruitment/'
             >
               Follow Us<FaLinkedin size={30} />
             </a>
