6005106e2e270b614e0d190797fea004f51ce908
Rizwan Ashraf <rizwanashraf@rizwans-mini.lan>
2024-03-05 22:41:15 +0000

    image and spelling

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import learn from '../assets/learn.png';
-import vip from '../assets/vip.png';
+import balloons from '../assets/balloons.png';
 import engage from '../assets/engage.png';
 
 const Companies = () => {
@@ -16,7 +16,7 @@ const Companies = () => {
           </div>
           </div>
           <div> 
-            <p className='text-gray-300 py-6 text-center'>We help companies attract the best technical talent</p>
+            <p className='text-gray-300 py-6 text-center'>We help companies attract the best technical talent.</p>
             </div>
             <div className='pt-8 max-w-[1000px] w-full grid sm:grid-cols-3 gap-8 px-4'>
             <div>
@@ -24,21 +24,21 @@ const Companies = () => {
             style={{ backgroundImage: `url(${learn})` }}
             className='shadow-lg shadow-[#040c16] mx-auto profile-div mt-6 mb-6 hover:scale-110 duration-500'>
             </div>
-            <p className='pt-8 text-center text-[#dbb434]'>LEARN</p><p className='pt-2 text-center'>Throughout your company's journey and culture, from inception to where you are now and where you aim to be, we strive to comprehend every aspect. Exceptional recruiters enhance brands, and we're dedicated to advocating for yours.</p>
+            <p className='pt-8 text-center text-[#dbb434]'>LEARN</p><p className='pt-2 text-center'>Throughout your company's journey, from inception to where you are now and where you aim to be, we strive to comprehend every aspect. Exceptional recruiters enhance brands, and we're dedicated to advocating for yours.</p>
             </div>
             <div>
             <div
             style={{ backgroundImage: `url(${engage})` }}
             className='shadow-lg shadow-[#040c16] mx-auto profile-div mt-6 mb-6 hover:scale-110 duration-500'>
             </div>
-            <p className='pt-8 text-center text-[#dbb434]'>ENGAGE</p><p className='pt-2 text-center'>We'll ensure that your proposition is communicated effectively, reaching the appropriate audience at the perfect moment. You'll exclusively encounter candidates who meet your technical and cultural requirements.</p>
+            <p className='pt-8 text-center text-[#dbb434]'>ENGAGE</p><p className='pt-2 text-center'>We'll ensure that your requirements are communicated effectively, reaching out to the appropriate talent. You'll only engage with candidates who meet your technical and cultural requirements.</p>
             </div>
             <div>
             <div
-            style={{ backgroundImage: `url(${vip})` }}
+            style={{ backgroundImage: `url(${balloons})` }}
             className='shadow-lg shadow-[#040c16] mx-auto profile-div mt-6 mb-6 hover:scale-110 duration-500'>
             </div>
-            <p className='pt-8 text-center text-[#dbb434]'>DELIVER</p><p className='pt-2 text-center'>There's no greater accomplishment for us than connecting individuals who significantly impact your business. New hires are your VIPs, and we're committed to being their dedicated guides.</p>
+            <p className='pt-8 text-center text-[#dbb434]'>DELIVER</p><p className='pt-2 text-center'>There's no greater accomplishment for us than connecting individuals who significantly impact your business. Our commitment to delivering exceptional candidates ensures that our clients thrive with the right talent by their side.</p>
             </div>
           </div>
       </div>
